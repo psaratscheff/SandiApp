9dc472ba65c9205dcead631cb3be2f4be65c730b
psaratscheff <pasaratscheff@uc.cl>
2015-10-29 23:36:56 -0300

    borrador (Por borrar)

--- app/src/main/java/cl/saratscheff/sandiapp/ComplaintActivity.java
+++ app/src/main/java/cl/saratscheff/sandiapp/ComplaintActivity.java
@@ -2,12 +2,38 @@ package cl.saratscheff.sandiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class ComplaintActivity extends AppCompatActivity {
+
+    ListView listViewDiscussion;
+    private Firebase nRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+        Firebase.setAndroidContext(this);
+
+        listViewDiscussion=(ListView)findViewById(R.id.listViewDiscussion);
+
+        nRef = new Firebase("https://sizzling-heat-8397.firebaseio.com/");
+
+        nRef.child("message").addValueEventListener(new ValueEventListener() {
+            int counter = 0;
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println("Contador: " + counter + "//" + snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+            }
+            @Override public void onCancelled(FirebaseError error) { }
+        });
+
+        nRef.child("message").child("message2").child("message3").setValue("Do you have data? You'll love Firebase.");
     }
 }
--- app/src/main/java/cl/saratscheff/sandiapp/LoginActivity.java
+++ app/src/main/java/cl/saratscheff/sandiapp/LoginActivity.java
@@ -3,6 +3,7 @@ package cl.saratscheff.sandiapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -63,7 +64,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-    private Firebase nRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -191,9 +191,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+
+            Intent intent = new Intent(this, ComplaintActivity.class);
+            startActivity(intent);
+
+            /* MODIFICACION TEMPORAL!!!! SE PUEDE BORRAR :)
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            mAuthTask.execute((Void) null);*/
         }
     }
 
