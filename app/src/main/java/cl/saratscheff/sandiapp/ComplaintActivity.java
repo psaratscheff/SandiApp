48238e540a19475708e6bbb0b0f89f946c7369d7
psaratscheff <pasaratscheff@uc.cl>
2015-11-02 17:04:32 -0300

    enviar mensajes

--- app/src/main/java/cl/saratscheff/sandiapp/ComplaintActivity.java
+++ app/src/main/java/cl/saratscheff/sandiapp/ComplaintActivity.java
@@ -2,38 +2,58 @@ package cl.saratscheff.sandiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ComplaintActivity extends AppCompatActivity {
 
     ListView listViewDiscussion;
     private Firebase nRef;
+    private String complaintID;
+    private Firebase messagesRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
-        Firebase.setAndroidContext(this);
-
         listViewDiscussion=(ListView)findViewById(R.id.listViewDiscussion);
 
+        Firebase.setAndroidContext(this);
         nRef = new Firebase("https://sizzling-heat-8397.firebaseio.com/");
+        complaintID = "edsgeac2BPLpgqtFd6zTnQsnqhiqeH3L";
+        messagesRef = nRef.child("markers").child(complaintID).child("messages");
 
-        nRef.child("message").addValueEventListener(new ValueEventListener() {
-            int counter = 0;
-
+        messagesRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                System.out.println("Contador: " + counter + "//" + snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
             }
             @Override public void onCancelled(FirebaseError error) { }
         });
-
-        nRef.child("message").child("message2").child("message3").setValue("Do you have data? You'll love Firebase.");
     }
+
+    public void sendMessage(View view) {
+        EditText editTextMessage = (EditText) findViewById(R.id.editTextMessage);
+        String msg = editTextMessage.getText().toString();
+        editTextMessage.getText().clear();
+
+        Firebase messagePost = messagesRef.push();
+        Map<String, String> post1 = new HashMap<String, String>();
+        post1.put("author", "USUARIO");
+        post1.put("content", msg);
+        messagePost.setValue(post1);
+    }
+
+
 }
